4dbbc8fa28c4843dab4511231e725f7b1d8b0c30
manuelrojas19 <manuelarr99@gmail.com>
2022-03-21 10:02:26 -0600

    add: Film and BaseEntity added

---
+++ src/main/java/com/ibm/academy/cms/filmservice/entity/BaseEntity.java
@@ -0,0 +1,48 @@
+package com.ibm.academy.cms.filmservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public abstract class BaseEntity {
+
+    @Id
+    private ObjectId id;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @LastModifiedDate
+    private Date lastModifiedAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getVersion(), that.getVersion());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getVersion());
+    }
+}
---
+++ src/main/java/com/ibm/academy/cms/filmservice/entity/Film.java
@@ -0,0 +1,29 @@
+package com.ibm.academy.cms.filmservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Document
+public class Film extends BaseEntity {
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+    private Date date;
+
+}
