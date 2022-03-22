ad38dad755cb4d3d1a0f1722957efe10f599a0cb
manuelrojas19 <manuelarr99@gmail.com>
2022-03-22 09:07:43 -0600

    add: PersonService and ActorService implmentations added

---
+++ src/main/java/com/ibm/academy/cms/filmservice/service/impl/ActorServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ibm.academy.cms.filmservice.service.impl;
+
+import com.ibm.academy.cms.filmservice.entity.Actor;
+import com.ibm.academy.cms.filmservice.entity.Person;
+import com.ibm.academy.cms.filmservice.repository.PersonRepository;
+import com.ibm.academy.cms.filmservice.service.ActorService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActorServiceImpl extends PersonServiceImpl implements ActorService {
+
+    @Autowired
+    public ActorServiceImpl(@Qualifier("actorRepository") PersonRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public Person update(ObjectId id, Person actor) {
+        Actor actorToUpdate = (Actor) this.findById(id);
+        actorToUpdate.setName(actor.getName());
+        actorToUpdate.setDescription(actor.getDescription());
+        actorToUpdate.setBornDate(actor.getBornDate());
+        actorToUpdate.setBornPlace(actor.getBornPlace());
+        return repository.save(actorToUpdate);
+    }
+}
