74715f3785d27e7cca6f75166554a091eb64d74a
manuelrojas19 <manuelarr99@gmail.com>
2022-03-22 09:06:42 -0600

    add: Person abstract repository and Actor Repository added

---
+++ src/main/java/com/ibm/academy/cms/filmservice/repository/ActorRepository.java
@@ -0,0 +1,7 @@
+package com.ibm.academy.cms.filmservice.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActorRepository extends PersonRepository {
+}
